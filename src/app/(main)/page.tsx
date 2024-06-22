c937cab65456077bdd7d2fef1d7877385fb34d1b
ashensam <iamashen27@gmail.com>
2024-06-22 19:01:47 +0530

    feat:minor commit

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,9 @@ import HeroSection from "./_components/hero-section";
 export default function Home() {
   return (
     <>
+
       <HeroSection />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In, totam.
     </>
   );
 }
